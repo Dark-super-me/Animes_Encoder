8e6d1e9d6a3e285ae4f04688188c1c413a700fb9
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-12 19:49:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # (c) @Animes_Encoded, Dark-super-me =!
+from telethon import events
+from config import bot
+from FastTelethonhelper import Timer, fast_upload, fast_download, Timer
+import subprocess
+import asyncio
