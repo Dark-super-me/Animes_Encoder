ac32de05f9c19332ec95d617b524524c1a1a7369
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-12 20:55:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,16 @@ async def _(event):
    
 @bot.on(events.NewMessage(pattern="/request_new_job"))
 async def _(event):
-    if Locked == False:
-        msg = await event.get_reply_message()
+    msg = await event.get_reply_message()
         r = await event.reply("Downloading..")
         file = await fast_download(bot, msg, r, "")
         gg = await event.client.get_entity(user.id)
         name = f"[{get_display_name(gg)}](tg://user?id={user.id})"
         await r.edit(f"Encoding The Video File for the user : {user}, check stats via /stats ...")
-        cmd = await bot.get_messages(FFMPEG, ids=FFMPEGCMD)
-        command = cmd.text.replace('[file]', file)
-        await event.reply(command)
-        asyncio.get_event_loop().run_until_complete(subprocess.call(f'./{command}', shell=True))
+        subprocess.call(f'./ffmpegFDK -i "{file}" -map 0 -c:v libx265 -vf scale=854:480 -crf 32  -c:a libfdk_aac  -profile:a aac_he_v2 -vbr 2 "[AG] {file}"', shell=True)
         await asyncio.sleep(1)
         res_file = await fast_upload(bot, f"[AG] {file}", r)
         await event.reply(file=res_file, force_document=True)
+    
+
     
