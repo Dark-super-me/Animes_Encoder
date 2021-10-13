597218706cd2fed4861817e04686ec9790aa6310
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-13 16:49:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ loop = asyncio.get_event_loop()
 async def _(event):
     await event.reply("I am alive master 😀")
 
-@bot.on(events.NewMessage(pattern="/stats"))
+@bot.on(events.NewMessage(pattern="/ls"))
 async def _(event):
     p = subprocess.Popen(f'ls -lh .', stdout=subprocess.PIPE, shell=True)
     await event.reply(p.communicate()[0].decode("utf-8", "replace").strip())
@@ -20,9 +20,7 @@ async def _(event):
     msg = await event.get_reply_message()
     r = await event.reply("Downloading..")
     file = await fast_download(bot, msg, r, "")
-    gg = await event.client.get_entity(user.id)
-    name = f"[{get_display_name(gg)}](tg://user?id={user.id})"
-    await r.edit(f"Encoding The Video File for the user : {name}, check stats via /stats ...")
+    await r.edit("Encoding The Video File Now , try using /ls ...")
     subprocess.call(f'./ffmpegFDK -i "{file}" -map 0 -c:v libx265 -vf scale=854:480 -crf 32  -c:a libfdk_aac  -profile:a aac_he_v2 -vbr 2 "[AG] {file}"', shell=True)
     await asyncio.sleep(1)
     res_file = await fast_upload(bot, f"[AG] {file}", r)
