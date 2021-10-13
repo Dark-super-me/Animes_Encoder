d0b52ba31b0f89a0b4ea640cc703d63e7b225b09
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-13 08:34:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def _(event):
     file = await fast_download(bot, msg, r, "")
     gg = await event.client.get_entity(user.id)
     name = f"[{get_display_name(gg)}](tg://user?id={user.id})"
-    await r.edit(f"Encoding The Video File for the user : {user}, check stats via /stats ...")
+    await r.edit(f"Encoding The Video File for the user : {name}, check stats via /stats ...")
     subprocess.call(f'./ffmpegFDK -i "{file}" -map 0 -c:v libx265 -vf scale=854:480 -crf 32  -c:a libfdk_aac  -profile:a aac_he_v2 -vbr 2 "[AG] {file}"', shell=True)
     await asyncio.sleep(1)
     res_file = await fast_upload(bot, f"[AG] {file}", r)
